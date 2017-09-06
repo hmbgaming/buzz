49e832d7295ef8b95cc94f2b12515675def96e6d
Darryl <darryl.github@gmail.com>
2017-09-06 09:46:01 -0700

    inital-branch-commit

--- buzzBlocks/game_roles/role.js
+++ buzzBlocks/game_roles/role.js
@@ -36,11 +36,20 @@ function create(conf, message, option) {
     message.author.send('You have created ' + role.slice(0, -1) + '!');
   }
 }
+function remove(conf, message, option) {
+  var role = ''; for (i=1; i < option.length; i++) {role += option[i]+' '};
+  var server_role = message.guild.roles.find("name", role.slice(0, -1));
+  server_role.delete()
+  .then(r => console.log(`Deleted role ${r}`))
+  .catch(console.error);
 
+}
 module.exports = {
   handler: function(conf, message, option) {
     if (option[0] == 'join') {join(conf, message, option)}
     if (option[0] == 'quit') {leave(conf, message, option)}
     if (option[0] == 'create') {create(conf, message, option)}
+
+    if (option[0] == 'remove') {remove(conf, message, option)}
   }
 };
