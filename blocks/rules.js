52b9b4b5ee166e5f7c8249052eee0cdb53e699f5
Darryl <darryl.github@gmail.com>
2017-09-18 19:53:26 -0700

    update

--- blocks/rules.js
+++ blocks/rules.js
@@ -7,10 +7,10 @@ module.exports = {
       .setThumbnail("https://cdn2.iconfinder.com/data/icons/helmet/512/warrior-soldier-helmet-war-512.png")
       .setTimestamp()
       .setDescription(`*All community members are expected to abide by the follow rules*`)
-      .addField("Rule 1", 'Treat others with respect. Banter is fun, bullying is not. Make sure all parties are on the same page when it comes to communicating. This includes both text and voice chats.')
-      .addField("Rule 2", 'Do not spam any channel with nonsense.')
-      .addField("Rule 3", 'No advertising. If you have an event, or something you would like to promote, please talk to a member of leadership to get permission.')
-      .addField("Rule 4", 'Leave all hate, racism, derogatory terms, and offensive comments out of our community.')
+      .addField("Rule 1", '*Treat others with respect. Banter is fun, bullying is not. Make sure all parties are on the same page when it comes to communicating. This includes both text and voice chats.*')
+      .addField("Rule 2", '*Do not spam any channel with nonsense.*')
+      .addField("Rule 3", '*No advertising. If you have an event, or something you would like to promote, please talk to a member of leadership to get permission.*')
+      .addField("Rule 4", '*Leave all hate, racism, derogatory terms, and offensive comments out of our community.*')
       .setFooter("Buzz");
     message.author.send(embed);
   }
