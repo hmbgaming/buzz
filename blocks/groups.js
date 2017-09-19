d842eb0c917412007717af7a5be91f114214b90a
Darryl <darryl.github@gmail.com>
2017-09-18 19:47:51 -0700

    in progress

--- blocks/groups.js
+++ blocks/groups.js
@@ -1,18 +1,14 @@
 
-function help(conf, message) {
-  let available_groups = ''; for (var role in conf['group_roles']) {available_groups += conf['group_roles'][role]+', '}
-  let embed = {embed: {
-    color: 3447003,
-    title: "Groups",
-    description: (`You can join game roles to send and receive games specific mentions!
-                   I'm very intuitive, so just let me know what game you wish to join!`),
-    fields: [{
-        name: "Available",
-        value: '```'+available_groups.slice(0,-2)+'```'},
-    ],
-    timestamp: new Date(),
-    footer: {text: "Buzz"}
-  }}
+function help(conf, message, discord) {
+  let available_groups = ''; for (var role in conf['group_roles']) {available_groups += conf['group_roles'][role]+'\n'}
+  let embed = new discord.RichEmbed()
+    .setColor(0x00AE86)
+    .setTitle("Group Notifications")
+    .setThumbnail("https://cdn2.iconfinder.com/data/icons/helmet/512/warrior-soldier-helmet-war-512.png")
+    .setTimestamp()
+    .setDescription(`*You can join game roles to send and receive games specific mentions! I'm very intuitive, so just let me know what game you wish to join!*`)
+    .addField("Available", '```'+available_groups.slice(0,-1)+'```')
+    .setFooter("Buzz");
   message.author.send(embed); return;
 }
 
@@ -36,12 +32,12 @@ function quit(conf, message, intent, value) {
 }
 
 module.exports = {
-  handler: (conf, message, intent, value, bot) => {
+  handler: (conf, message, intent, value, discord) => {
     if (message.channel.type === 'text'){
       if (intent === 'group_join') {join(conf, message, intent, value); return}
       if (intent === 'group_quit') {quit(conf, message, intent, value); return}
-      if (intent === 'group_help') {help(conf, message); return}
+      if (intent === 'group_help') {help(conf, message, discord); return}
     } else {
-      if (intent === 'group_help') {help(conf, message); return}
+      if (intent === 'group_help') {help(conf, message, discord); return}
       message.author.send("Attempted to interact with "+value+", but you are in a DM channel. Please try again in the server.")}
 }};
