cef8c399414f745ccf5a7fa9309493310f60e34a
Darryl <darryl.github@gmail.com>
2017-09-19 22:01:51 -0700

    in progress

--- blocks/reddit.js
+++ blocks/reddit.js
@@ -1,4 +1,4 @@
-function watcher(bot, discord, conf, feed, feed_name, color) {
+function watcher(bot, discord, conf, feed, feed_name, color, channel) {
   let RssFeedEmitter = require('rss-feed-emitter');
   let feeder = new RssFeedEmitter();
 
@@ -11,13 +11,13 @@ function watcher(bot, discord, conf, feed, feed_name, color) {
       .setFooter(feed_name)
       .setTimestamp()
       .setURL(item.link);
-    bot.channels.find('name', conf['game-deal-channel']).send({embed});
+    bot.channels.find('name', channel).send({embed});
 });
 }
 
 module.exports = {
   handler: (bot, discord, conf) => {
-    watcher(bot, discord, conf, 'https://www.reddit.com/r/gametrailers/new.rss', 'Game Trailers', '#AE0086');
-    watcher(bot, discord, conf, 'https://www.reddit.com/r/GameDeals/new.rss', 'Game Deals', '#00AE86');
+    watcher(bot, discord, conf, 'https://www.reddit.com/r/gametrailers/new.rss', 'Game Trailers', '#AE0086', conf['game-deal-channel']);
+    watcher(bot, discord, conf, 'https://www.reddit.com/r/GameDeals/new.rss', 'Game Deals', '#00AE86', conf['game-trailer-channel']);
   }
 }
