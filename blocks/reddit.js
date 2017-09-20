9170576077c3ad7dadf4e3191ce53936204ca85f
Darryl <darryl.github@gmail.com>
2017-09-19 22:54:16 -0700

    in progress

--- blocks/reddit.js
+++ blocks/reddit.js
@@ -1,23 +1,29 @@
-function watcher(bot, discord, conf, feed, feed_name, color, channel) {
-  let RssFeedEmitter = require('rss-feed-emitter');
-  let feeder = new RssFeedEmitter();
-
-  feeder.add({url: feed, refresh: 60 });
-  feeder.on('new-item', function(item) {
-    let embed = new discord.RichEmbed()
-      .setColor(color)
-      .setTitle(feed_name)
-      .setDescription(item.title)
-      .setFooter(feed_name)
-      .setTimestamp()
-      .setURL(item.link);
-    bot.channels.find('name', channel).send({embed});
-});
+function reddit_search(bot, reddit_client, sub_reddit, display_name, discord, conf) {
+  var watch = reddit_client.watcher.getPostWatcher(sub_reddit).
+  watch.on('post', function(post) {
+      let embed = new discord.RichEmbed()
+        .setColor(0x00AE86)
+        .setTitle(display_name)
+        .setDescription(post.data.title)
+        .setFooter(display_name)
+        .setTimestamp()
+        .setURL(post.data.url);
+      bot.channels.find('name', conf['game-deal-channel']).send({embed});
+    });
 }
 
 module.exports = {
-  handler: (bot, discord, conf) => {
-    watcher(bot, discord, conf, 'https://www.reddit.com/r/gametrailers/new.rss', 'Game Trailers', '#AE0086', conf['game-deal-channel']);
-    watcher(bot, discord, conf, 'https://www.reddit.com/r/GameDeals/new.rss', 'Game Deals', '#00AE86', conf['game-trailer-channel']);
+  handler: (bot, reddit, discord, conf) => {
+    let reddit_client = new reddit({
+      username: process.env.REDDIT_USERNAME,
+      password: process.env.REDDIT_PASSWORD,
+      app_id: process.env.REDDIT_ID,
+      api_secret: process.env.REDDIT_SECRET,
+
+      automatic_retries: true,
+      api_requests_per_minuite: 10
+    });
+    reddit_search(bot, reddit_client, 'popular', 'Game Trailers', discord, conf);
+    //reddit_search(bot, reddit_client, 'GameDeals', 'Game Deals', discord, conf);
   }
 }
