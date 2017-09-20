6a7add15890124cf05db1651635687a8b0879a11
Darryl <darryl.github@gmail.com>
2017-09-19 20:58:18 -0700

    in progress

--- blocks/reddit.js
+++ blocks/reddit.js
@@ -1,45 +1,23 @@
-function watcher(bot, conf) {
-  let embed = new discord.RichEmbed()
-    .setColor(0x00AE86)
-    .setTitle('')
-    .setDescription('')
-    .setFooter('')
-    .setTimestamp()
-    .setURL('');
-  bot.channels.find('name', conf['game-deal-channel']).send({embed});
+function watcher(bot, discord, conf, feed, feed_name) {
+  let RssFeedEmitter = require('rss-feed-emitter');
+  let feeder = new RssFeedEmitter();
+
+  feeder.add({url: feed, refresh: 2000 });
+  feeder.on('new-item', function(item) {
+    let embed = new discord.RichEmbed()
+      .setColor(0x00AE86)
+      .setTitle(feed_name)
+      .setDescription(item.title)
+      .setFooter(feed_name)
+      .setTimestamp()
+      .setURL(item.link);
+    bot.channels.find('name', conf['game-deal-channel']).send({embed});
+});
 }
 
-
-
-
 module.exports = {
-  watcher: (bot, discord, conf) => {
-    let RssFeedEmitter = require('rss-feed-emitter');
-    let feeder = new RssFeedEmitter();
-
-    //feeder.add({url: 'https://www.reddit.com/r/GameDeals/new.rss',
-      //refresh: 2000 });
-    feeder.add({url: 'https://www.reddit.com/r/gametrailers/new.rss',
-      refresh: 2000 });
-
-    feeder.on('new-item', function(item) {
-      console.log(item);
-      process.exit();
-
-      let embed = new discord.RichEmbed()
-        .setColor(0x00AE86)
-        .setTitle('')
-        .setDescription('')
-        .setFooter('')
-        .setTimestamp()
-        .setURL('');
-      bot.channels.find('name', conf['game-deal-channel']).send({embed});
-    })
+  handler: (bot, discord, conf) => {
+    watcher(bot, discord, conf, 'https://www.reddit.com/r/gametrailers/new.rss', 'Game Trailers');
+    watcher(bot, discord, conf, 'https://www.reddit.com/r/GameDeals/new.rss', 'Game Deals');
   }
-
-
-
-
-
-
 }
