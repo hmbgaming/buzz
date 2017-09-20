64d92906673ffc826836a730ee9da569d9597ef6
Darryl <darryl.github@gmail.com>
2017-09-19 21:09:24 -0700

    in progress

--- blocks/reddit.js
+++ blocks/reddit.js
@@ -1,11 +1,11 @@
-function watcher(bot, discord, conf, feed, feed_name) {
+function watcher(bot, discord, conf, feed, feed_name, color) {
   let RssFeedEmitter = require('rss-feed-emitter');
   let feeder = new RssFeedEmitter();
 
   feeder.add({url: feed, refresh: 2000 });
   feeder.on('new-item', function(item) {
     let embed = new discord.RichEmbed()
-      .setColor(0x00AE86)
+      .setColor(color)
       .setTitle(feed_name)
       .setDescription(item.title)
       .setFooter(feed_name)
@@ -17,7 +17,7 @@ function watcher(bot, discord, conf, feed, feed_name) {
 
 module.exports = {
   handler: (bot, discord, conf) => {
-    watcher(bot, discord, conf, 'https://www.reddit.com/r/gametrailers/new.rss', 'Game Trailers');
-    watcher(bot, discord, conf, 'https://www.reddit.com/r/GameDeals/new.rss', 'Game Deals');
+    watcher(bot, discord, conf, 'https://www.reddit.com/r/gametrailers/new.rss', 'Game Trailers', '#AE0086');
+    watcher(bot, discord, conf, 'https://www.reddit.com/r/GameDeals/new.rss', 'Game Deals', '#00AE86');
   }
 }
