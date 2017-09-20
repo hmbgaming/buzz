29d9d7beda08654303b958e7c7e2d5c3c3f3dea8
Darryl <darryl.github@gmail.com>
2017-09-20 16:19:28 -0700

    in progress

--- blocks/release.js
+++ blocks/release.js
@@ -6,18 +6,19 @@ function random(min, max) {
 }
 
 function release_key(moment, schedule, database, conf) {
+  conf['daily-key-release'] = moment().format('LL');
+
   let release_hour = random(8, 22);
   let release_minute = random(1, 59);
   //let release_date = `* ${release_minute} ${release_hour} * * *`
-  conf['daily-key-release'] = moment().format('LL');
-
   let release_date = `1 * * * * *`
 
 
   var j = schedule.scheduleJob(release_date, function(){
     var collection = database.collection('release-keys');
     collection.find({}).toArray(function(err, table) {
-      for (let row in table) {console.log(table[row]['key'])}//process.env.CURRENT_RELEASE = table[row]['key']; return}
+      for (let row in table) {var new_release = table[row]['key']; return}//process.env.CURRENT_RELEASE = table[row]['key']; return}
+      console.log(new_release);
     });
 
     j.cancel();
