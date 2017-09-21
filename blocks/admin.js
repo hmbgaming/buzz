b9bb90fe1e041c0dc43dda42a5ae90850d98900d
Darryl <darryl.github@gmail.com>
2017-09-20 19:58:03 -0700

    in progress

--- blocks/admin.js
+++ blocks/admin.js
@@ -2,7 +2,6 @@
 module.exports = {
   uptime: (conf, client, message) => {
     if (message.channel.type === 'dm') {message.author.send({embed: {color: 3447003, title: 'Server commands must be used in the server.',}}); return}
-    if (message.member.roles.some(r=>[conf['admin-role']].includes(r.name)) === false) {return;}
     message.author.send({embed: {color: 3447003, title: ((client.uptime/1000.0)/60).toFixed(2)+" minutes!",}});
   },
 
@@ -21,5 +20,5 @@ module.exports = {
         message.guild.createRole(new_role);
 
   }} message.author.send({embed: {color: 3447003, title: 'Success',}})},
-  
+
 }
