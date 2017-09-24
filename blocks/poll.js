b5089e6e1ad23d217b8d6bd5948802baf4aa478b
Darryl <darryl.github@gmail.com>
2017-09-23 20:10:04 -0700

    in progress

--- blocks/poll.js
+++ blocks/poll.js
@@ -2,23 +2,17 @@
 module.exports = {
   create: (conf, bot, discord, message, option) => {
     if (message.channel.type === 'dm') {message.author.send({embed: {color: 3447003, title: 'Server commands must be used in the server.',}}); return}
-    //message.delete();
+    message.delete();
     if (message.member.roles.some(r=>[conf['admin-role']].includes(r.name)) === false) {return;}
 
     let question=''; for (i=1; i < option.length; i++) {question += option[i]+' '}
     let embed = new discord.RichEmbed()
       .setColor('#8600AE')
       .setDescription(question)
-      .setFooter('Polls')
-      .setTimestamp();
     message.channel.send({embed}).then(msg => {
       msg.react('👍');
       msg.react('👎');
       msg.pin()
     });
-
-    //bot.user.lastMessage.react(':thumbsup:');
-    //bot.user.lastMessage.react(':thumbsdown:');
-    //bot.user.lastMessage.pin();
   }
 }
