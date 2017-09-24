b9f58e29dda9bf9885308aef0d231fde5ee949d3
Darryl <darryl.github@gmail.com>
2017-09-24 10:31:43 -0700

    in progress

--- buzz.js
+++ buzz.js
@@ -31,20 +31,6 @@ airbrake.addFilter(function(notice) {
 });
 airbrake.handleExceptions();
 
-
-const help_fallback = new discord.RichEmbed()
-  .setColor(0x00AE86)
-  .setTitle("Group Notifications")
-  .setThumbnail("https://cdn2.iconfinder.com/data/icons/helmet/512/warrior-soldier-helmet-war-512.png")
-  .setTimestamp()
-  .setFooter("Buzz")
-  .setDescription()
-  .addField('Group Notifications','You can *join* roles to recieve game specific messages no matter what channel they are in!')
-  .addField('Leveling','As you communicate with your peers you will accumulate experience, as you level up you will be rewarded with new roles & privileges')
-  .addField('Channel Polls','Make sure to check a channels pinned messages for the current polls going on!')
-  .addField('Releasing Game Keys','Between 8am - 10pm, there is a chance for a steam game key to be released in <text-channel>!');
-
-
 function handler(message) {
   let request = ai.textRequest(message.content, {sessionId: message.author.username});
   request.on('response', function(response) {
@@ -57,7 +43,19 @@ function handler(message) {
 
       if (response['result']['action'] === 'group') {groups.handler(conf, message, intent, discord_response, discord); return}
       if (response['result']['action'] === 'rules') {rules.handler(message, discord); return}
-      if (response['result']['action'] === 'help-fallback') {message.author.send({help_fallback}); return}
+      if (response['result']['action'] === 'help-fallback') {
+        let help_fallback = new discord.RichEmbed()
+          .setColor(0x00AE86)
+          .setTitle("Group Notifications")
+          .setThumbnail("https://cdn2.iconfinder.com/data/icons/helmet/512/warrior-soldier-helmet-war-512.png")
+          .setTimestamp()
+          .setFooter("Buzz")
+          .setDescription()
+          .addField('Group Notifications','You can *join* roles to recieve game specific messages no matter what channel they are in!')
+          .addField('Leveling','As you communicate with your peers you will accumulate experience, as you level up you will be rewarded with new roles & privileges')
+          .addField('Channel Polls','Make sure to check a channels pinned messages for the current polls going on!')
+          .addField('Releasing Game Keys','Between 8am - 10pm, there is a chance for a steam game key to be released in <text-channel>!');
+        message.author.send(help_fallback); return}
 
       message.author.send(discord_response); return;
     }
