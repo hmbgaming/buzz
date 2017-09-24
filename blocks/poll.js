b2602577fe4823869835b731f0460ebaf62b4ac1
Darryl <darryl.github@gmail.com>
2017-09-23 18:38:46 -0700

    in progress

--- blocks/poll.js
+++ blocks/poll.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-  create: (conf, discord, message, question) => {
+  create: (conf, bot, discord, message, question) => {
     if (message.channel.type === 'dm') {message.author.send({embed: {color: 3447003, title: 'Server commands must be used in the server.',}}); return}
     //message.delete();
     if (message.member.roles.some(r=>[conf['admin-role']].includes(r.name)) === false) {return;}
@@ -10,8 +10,8 @@ module.exports = {
       .setFooter('Polls')
       .setTimestamp();
     message.channel.send({embed});
-    message.react(':thumbsup:');
-    message.react(':thumbsdown:');
-    message.pin();
+    bot.user.lastMessage.react(':thumbsup:');
+    bot.user.lastMessage.react(':thumbsdown:');
+    bot.user.lastMessage.pin();
   }
 }
